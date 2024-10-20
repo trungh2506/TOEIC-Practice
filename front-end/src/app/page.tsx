27a1ac442cbed7c4b27e15c4ee78d7c3ddf75171
trungh2506 <trungvipqn12@gmail.com>
2024-10-20 21:45:12 +0700

    init nextjs project, install shadCN UI library, setup pages

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Toeic Area !",
+};
+
+export default function Page() {
+  return <h1 className="quicksand-semibold">Hello, Home Page!</h1>;
+}
---
+++ front-end/src/app/practice/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Practice Page</h1>;
+}
